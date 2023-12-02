09e00e1903fcc291f2c5c3c823ac482f344b66b2
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-02 22:33:29 +0100

    day 2

--- day01.nim
+++ day01.nim
@@ -1,22 +1,11 @@
-import sequtils, strutils, tables, std/enumerate
+import sequtils, strutils, tables, std/enumerate, strformat
 
+let numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8,
+               "nine": 9}.toTable()
 
-let numberMap = {
-  "one": 1, 
-  "two": 2, 
-  "three": 3, 
-  "four": 4, 
-  "five": 5, 
-  "six": 6, 
-  "seven": 7, 
-  "eight": 8, 
-  "nine": 9,
-}.toTable()
-
-proc part12(fp: string, allow_words=false): int =
-    var
-        file = open(fp)
-        sum = 0
+proc part12(fp: string, allow_words: bool=false): int =
+    var file = open(fp)
+    
     for line in file.lines:
         var (first, last) = (-1, -1)
         for i, c in enumerate(line.toSeq):
@@ -26,19 +15,27 @@ proc part12(fp: string, allow_words=false): int =
                     first = n
                 last = n
             elif allow_words:
-                for k in numberMap.keys():
+                for k in numbers.keys():
                     if line.substr(i, line.len-1).startsWith(k):
-                        let n = numberMap[k]
+                        let n = numbers[k]
                         if first == -1:
                             first = n
                         last = n
 
-        sum += first * 10 + last
+        result += first * 10 + last
 
-    return sum
-                
-assert part12("inputs/01e1.txt") == 142
-assert part12("inputs/01a.txt") == 54951
-assert part12("inputs/01e2.txt", true) == 281
-assert part12("inputs/01a.txt", true) == 55218
+    return result
+
+const day = "01"
+assert part12(fmt"inputs/{day}e1.txt") == 142
+assert part12(fmt"inputs/{day}a.txt") == 54951
+assert part12(fmt"inputs/{day}e2.txt", true) == 281
+assert part12(fmt"inputs/{day}a.txt", true) == 55218
+
+
+
+
+
+
+
 
---
+++ day02.nim
@@ -0,0 +1,40 @@
+import strutils, tables, std/enumerate, strformat
+import std/sequtils
+
+let maxAllowed = {"red": 12, "green": 13, "blue": 14}.toTable()
+
+proc part1(fp: string): int =
+    var file = open(fp)
+    
+    for i, line in enumerate(file.lines):
+        var ok = true
+        for pair in line.split(":")[1].replace(";", ",").split(","):
+            let parts = pair.strip().split(" ")
+            let (n, color) = (parts[0].parseInt, parts[1])
+            if n > maxAllowed[color]:
+                ok = false
+
+        if ok:
+            result += i + 1
+
+    return result
+
+proc part2(fp: string): int =
+    var file = open(fp)
+    
+    for line in file.lines:
+        var minRequired = {"red": -99, "green": -99, "blue": -99}.toTable()
+        for pair in line.split(":")[1].replace(";", ",").split(","):
+            let parts = pair.strip().split(" ")
+            let (n, color) = (parts[0].parseInt, parts[1])
+            minRequired[color] = max(minRequired[color], n)
+
+        result += minRequired.values.toSeq.foldl(a * b)
+
+    return result
+
+const day = "02"
+assert part1(fmt"inputs/{day}e1.txt") == 8
+assert part1(fmt"inputs/{day}a.txt") == 2720
+assert part2(fmt"inputs/{day}e1.txt") == 2286
+assert part2(fmt"inputs/{day}a.txt") == 71535
