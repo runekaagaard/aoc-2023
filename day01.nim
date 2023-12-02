bda9a287b71cb3345fd800c10e0e6924b337e6ae
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-03 00:26:27 +0100

    cleanups

--- day01.nim
+++ day01.nim
@@ -1,7 +1,7 @@
 import std/sequtils, std/strutils, std/tables, std/enumerate, std/strformat
 
 let numbers = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8,
-               "nine": 9}.toTable()
+               "nine": 9}.toTable
 
 proc part12(fp: string, allow_words: bool=false): int =
     var file = open(fp)
@@ -9,13 +9,13 @@ proc part12(fp: string, allow_words: bool=false): int =
     for line in file.lines:
         var (first, last) = (-1, -1)
         for i, c in enumerate(line.toSeq):
-            if c.isDigit():
+            if c.isDigit:
                 let n = int(c) - int('0')
                 if first == -1:
                     first = n
                 last = n
             elif allow_words:
-                for k in numbers.keys():
+                for k in numbers.keys:
                     if line.substr(i, line.len-1).startsWith(k):
                         let n = numbers[k]
                         if first == -1:
@@ -23,8 +23,6 @@ proc part12(fp: string, allow_words: bool=false): int =
                         last = n
 
         result += first * 10 + last
-
-    return result
 
 const day = "01"
 assert part12(fmt"inputs/{day}e1.txt") == 142
--- day02.nim
+++ day02.nim
@@ -2,31 +2,27 @@ import std/sequtils, std/strutils, std/tables, std/enumerate, std/strformat
 
 iterator stones(line: string): (int, string) =
     for pair in line.split(":")[1].replace(";", ",").split(","):
-        let parts = pair.strip().split(" ")
+        let parts = pair.strip.split(" ")
         
         yield (parts[0].parseInt, parts[1])
 
 proc part1(fp: string): int =
     var file = open(fp)
-    const maxAllowed = {"red": 12, "green": 13, "blue": 14}.toTable()
+    const maxAllowed = {"red": 12, "green": 13, "blue": 14}.toTable
     
     for i, line in enumerate(file.lines):
         if not stones(line).toSeq.anyIt(it[0] > maxAllowed[it[1]]):
             result += i + 1
 
-    return result
-
 proc part2(fp: string): int =
     var file = open(fp)
     
     for line in file.lines:
-        var minRequired = {"red": -1, "green": -1, "blue": -1}.toTable()
+        var minRequired = {"red": -1, "green": -1, "blue": -1}.toTable
         for n, color in stones(line):
             minRequired[color] = max(minRequired[color], n)
 
         result += minRequired.values.toSeq.foldl(a * b)
-
-    return result
 
 const day = "02"
 assert part1(fmt"inputs/{day}e1.txt") == 8
