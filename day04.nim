8b6e5e81f7c7da59ccd3ee3a9f264da441498d01
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-04 18:25:36 +0100

    day 4 part 2

--- day04.nim
+++ day04.nim
@@ -5,23 +5,29 @@ type
 
 iterator parse(file: File): Row =
     proc makeNums(s: string): HashSet[int] =
-        collect(initHashSet()):
-            for x in s.split(" "):
-                if x.strip.len > 0:
-                    {x.parseInt}
+        collect:
+            for x in s.split(" ").filterIt(it.strip.len > 0): {x.parseInt}
                     
     for line in file.lines:
         let parts = line.split(":")[1].split("|")
         yield (makeNums(parts[0]), makeNums(parts[1]))
 
-proc part1(file: File): int =
-    file.parse.toSeq.mapIt((it.has * it.nums).len).filterIt(it > 0).mapIt(2 ^ (it - 1)).sum
+proc matches(file: File): seq[int] = file.parse.toSeq.mapIt((it.has * it.nums).len)
+proc part1(file: File):   int      = file.matches.filterIt(it > 0).mapIt(2 ^ (it - 1)).sum
     
-# proc part2(file: File): int =
-#     discard
+proc part2(file: File): int =
+    var ms = file.matches
+    var counts = collect(initTable(ms.len)):
+        for i in 0 .. ms.len - 1: {i: 1}
+
+    for i in 0 .. ms.len - 1:
+        for j in 0 .. ms[i] - 1:
+            counts[i+j+1] += counts[i]
+
+    counts.values.toSeq.sum
 
 const day = "04"
 assert part1(open(fmt"inputs/{day}e1.txt")) == 13
 assert part1(open(fmt"inputs/{day}i.txt")) == 33950
-# echo part2(open(fmt"inputs/{day}e2.txt"))
-# echo part2(open(fmt"inputs/{day}i.txt"))
+assert part2(open(fmt"inputs/{day}e2.txt")) == 30
+assert part2(open(fmt"inputs/{day}i.txt")) == 14814534
