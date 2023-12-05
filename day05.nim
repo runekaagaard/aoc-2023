7b879fb8693d167a521362c9d7f25d12a500fe51
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-05 20:09:38 +0100

    day 5 part 2

--- day05.nim
+++ day05.nim
@@ -3,10 +3,8 @@ import std/[sequtils, strutils, tables, enumerate, strformat, sugar, sets, hashe
 type
     Map = tuple[d, s, r: int]
 
-proc asMap(xs: seq): Map =
-    (xs[0], xs[1], xs[2])
-    
 proc parse(file: File): (seq[int], seq[seq[Map]]) =
+    proc asMap(xs: seq): Map = (xs[0], xs[1], xs[2])
     let seeds: seq[int] = file.read_line.split(": ")[1].split(" ").mapIt(it.strip).map(parseInt)
     var xs = file.lines.toSeq.filterIt(not it.endsWith(":")).join(".")
     var categories:  seq[seq[Map]]
@@ -16,34 +14,31 @@ proc parse(file: File): (seq[int], seq[seq[Map]]) =
         categories.add(map)
 
     return (seeds, categories)
+
+proc distance(seed: int, categories: seq[seq[Map]]): int =
+    result = seed
+    for i, category in categories:
+        for map in category:
+            if map.s <= result and result <= map.s + map.r - 1:
+                result = result + map.d - map.s
+                break
+
+    return result
     
 proc part1(file: File): int =
     let (seeds, categories) = parse(file)
-    var solutions: seq[int]
-    
-    for seed in seeds:
-        var num = seed
-        # echo "seed: ", num
-        for i, category in categories:
-            # echo "  cat: ", i
-            for map in category:
-                # echo "    map: ", map
-                if map.s <= num and num <= map.s + map.r - 1:
-                    num = num + map.d - map.s
-                    # echo "      new num: ", num
-                    break
-
-        solutions.add(num)
-
-    return solutions.min
-    
-        
+    seeds.mapIt(it.distance(categories)).min
     
 proc part2(file: File): int =
-    discard
+    let (seeds, categories) = parse(file)
+    result = int.high
+    
+    for pair in seeds.distribute(seeds.len div 2):
+        for seed in countup(pair[0], pair[0]+pair[1]-1):
+            result = result.min(distance(seed, categories))
 
 const day = "05"
 assert part1(open(fmt"inputs/{day}e1.txt")) == 35
 assert part1(open(fmt"inputs/{day}i.txt")) == 51752125
-# assert part2(open(fmt"inputs/{day}e2.txt")) == 30
-# assert part2(open(fmt"inputs/{day}i.txt")) == 14814534
+assert part2(open(fmt"inputs/{day}e1.txt")) == 46
+assert part2(open(fmt"inputs/{day}i.txt")) == 12634632
