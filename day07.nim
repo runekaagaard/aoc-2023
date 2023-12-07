d4c5373bed618816b57bc86fe50f2cf277506243
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-07 16:41:24 +0100

    cleanups

--- day07.nim
+++ day07.nim
@@ -3,7 +3,7 @@ import std/[sequtils, strutils, enumerate, strformat, sugar, sets, hashes, math,
 type
     Line = (string, int, int, int)
 
-const cardToHex = {'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6': 5,
+const cardToBase13 = {'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6': 5,
                    '5': 4, '4': 3, '3': 2, '2': 1}.toTable()
 
 let handTypes = @[@[5], @[1, 4], @[2, 3], @[1, 1, 3], @[1, 2, 2], @[1, 1, 1, 2], @[1, 1, 1, 1, 1]]
@@ -16,8 +16,8 @@ proc parseHand(hand: seq[int]): (int, int) =
 
     let handTypeIndex = handTypes.find(counts.values.toSeq.sortedByIt(it))
     let strenght = (
-        (handTypes.len - handTypeIndex) * (cardToHex.len ^ 6) +
-        countdown(hand.len-1, 0).toSeq.mapIt(cardToHex.len ^ (hand.len-it) * hand[it]).sum
+        (handTypes.len - handTypeIndex) * (cardToBase13.len ^ (hand.len+1)) +
+        countdown(hand.len-1, 0).toSeq.mapIt(cardToBase13.len ^ (hand.len-it) * hand[it]).sum
     )
     
     (handTypeIndex, strenght)
@@ -25,7 +25,7 @@ proc parseHand(hand: seq[int]): (int, int) =
 proc parse(file: File): seq[Line] =
     for line in file.lines:
         let xs = line.split(" ")
-        let hand = xs[0].mapIt(cardToHex[it])
+        let hand = xs[0].mapIt(cardToBase13[it])
         let (handType, strenght) = hand.parseHand
         result.add((xs[0], xs[1].parseInt, strenght, handType))
         
