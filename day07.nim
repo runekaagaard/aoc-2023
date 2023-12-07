a91c2449bb74cf9ebd7331f6f185f8a81c35c34e
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-07 16:06:13 +0100

    day 7 part 1

--- day07.nim
+++ day07.nim
@@ -1,4 +1,4 @@
-import std/[sequtils, strutils, tables, enumerate, strformat, sugar, sets, hashes, math, options, strscans, algorithm]
+import std/[sequtils, strutils, enumerate, strformat, sugar, sets, hashes, math, options, strscans, algorithm, tables]
 
 type
     Line = (string, int, int, int)
@@ -6,21 +6,22 @@ type
 const cardToHex = {'A': 13, 'K': 12, 'Q': 11, 'J': 10, 'T': 9, '9': 8, '8': 7, '7': 6, '6': 5,
                    '5': 4, '4': 3, '3': 2, '2': 1}.toTable()
 
-let handTypes = @[[5].toHashSet, [4].toHashSet, [3, 2].toHashSet, [3].toHashSet, [2, 2].toHashSet,
-                    [2].toHashSet, initHashSet[int]()]
-
-echo handTypes.find(@[3, 2].toHashSet)
+let handTypes = @[@[5], @[1, 4], @[2, 3], @[1, 1, 3], @[1, 2, 2], @[1, 1, 1, 2], @[1, 1, 1, 1, 1]]
 
 proc handStrength(hand: seq[int]): (int, int) =
-    var handType = initHashSet[int]()
-    for card in hand.toHashSet:
+    var counts = initTable[int, int]()
+    for card in hand:
         let count = hand.countIt(it == card)
-        if count == 1:
-            continue
-        handType.incl(count)
+        counts[card] = count
 
-    # echo "    ", handType, " ", handTypes.find(handType), " ", hand
-    (handTypes.find(handType), (6 - handTypes.find(handType)) * 1_000_000_000 + countdown(hand.len-1, 0).toSeq.mapIt(
+    
+    let handType = counts.values.toSeq.sortedByIt(it)
+    let index = handTypes.find(handType)
+    if index == -1:
+         echo "missing", handType
+    # assert index != -1
+    # # echo "    ", handType, " ", handTypes.find(handType), " ", hand
+    (index, (6 - index) * 1_000_000_000 + countdown(hand.len-1, 0).toSeq.mapIt(
         16^(hand.len-it)*hand[it]).sum)
 
 proc parse(file: File): seq[Line] =
@@ -33,12 +34,12 @@ proc parse(file: File): seq[Line] =
         
 proc part1(file: File): int =
     for i, (hand, bet, strength, handType) in file.parse.sortedByIt(it[2]):
-        #echo i, " ", (hand, bet, strength), " ", handType, " ", hand.mapIt(cardToHex[it]+1)
-        echo handType, " ", hand.mapIt(cardToHex[it]+1)
+        echo i, " ", (hand, bet, strength), " ", handType, " ", hand.mapIt(cardToHex[it]+1)
+        # echo handType, " ", hand.mapIt(cardToHex[it]+1)
         result += (i+1) * bet
     
 const day = "07"
-#echo part1(open(fmt"inputs/{day}e1.txt"))
+echo part1(open(fmt"inputs/{day}e1.txt"))
 echo part1(open(fmt"inputs/{day}i.txt")) # 252008590
 # echo part2(open(fmt"inputs/{day}e1.txt"))
 # echo part2(open(fmt"inputs/{day}i.txt"))
