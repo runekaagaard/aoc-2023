99ceaf57c2a03731c953aee101a2df7c1cd3fc6c
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-08 17:54:56 +0100

    day 08 part 2

--- day08.nim
+++ day08.nim
@@ -1,6 +1,6 @@
 import std/[sequtils, strutils, enumerate, strformat, sugar, sets, hashes, math, options, strscans, algorithm, tables]
 
-proc part1(file: File): int =
+proc parse(file: File): (seq[string], Table[string, seq[string]]) =
     let cmds = file.readLine.toSeq.join(" ").split()
     discard file.readLine
 
@@ -10,46 +10,36 @@ proc part1(file: File): int =
         assert a notin graph
         graph[a] = @[b, c]
 
-    var cur = "AAA"
+    (cmds, graph)
+    
+proc part1(file: File, start: string="AAA"): int =
+    var (cmds, graph) = parse(file)
+    
+    var cur = start
     for i, cmd in cmds.cycle(100):
         assert cmd in @["L", "R"]
         var j: int = (if cmd == "L": 0 else: 1)
-        if cur == "ZZZ":
+        if cur.endsWith("Z"):
             return i
 
         cur = graph[cur][j]
         
-proc part2(file: File): int =
-    let cmds = file.readLine.toSeq.join(" ").split()
-    discard file.readLine
-
+proc part2(file_name: string): int =
+    var (_, graph) = parse(file_name.open)
     var curs: seq[string]
-    var graph = initTable[string, seq[string]]()
-    for line in file.lines:
-        var (a, b, c) = (line[0 .. 2], line[7 .. 9], line[12 .. 14])
-        assert a notin graph
-        graph[a] = @[b, c]
-
+    for a, _ in graph:
         if a.endsWith("A"):
             curs.add(a)
 
-    var i = -1
-    for _ in 0 .. 100:
-        for cmd in cmds:
-            i += 1
-            assert cmd in @["L", "R"]
-            var j: int = (if cmd == "L": 0 else: 1)
-            if curs.allIt(it.endsWith("Z")):
-                return i
+    var ns: seq[int]
+    for cur in curs:
+        ns.add(part1(file_name.open, start=cur))
 
-            for k in 0 ..< curs.len:
-                curs[k] = graph[curs[k]][j]
-
+    ns.foldl(lcm(a, b))
         
 const day = "08"
 assert part1(open(fmt"inputs/{day}e1.txt")) == 2
 assert part1(open(fmt"inputs/{day}e2.txt")) == 6
 assert part1(open(fmt"inputs/{day}i.txt")) == 22357
-assert part2(open(fmt"inputs/{day}e3.txt")) == 6
-# echo part2(open(fmt"inputs/{day}i.txt"))
-# assert part2(open(fmt"inputs/{day}i.txt")) == 253630098
+assert part2(fmt"inputs/{day}e3.txt") == 6
+assert part2(fmt"inputs/{day}i.txt") == 10371555451871
