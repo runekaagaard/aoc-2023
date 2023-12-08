f89a1970724018776f732325314500772bad6cab
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-08 16:54:06 +0100

    work on part 2

--- day08.nim
+++ day08.nim
@@ -6,9 +6,8 @@ proc part1(file: File): int =
 
     var graph = initTable[string, seq[string]]()
     for line in file.lines:
-        var a, b, c: string
-        var ok = scanf(line, "$w = ($w, $w)", a, b, c)
-        assert ok and a notin graph
+        var (a, b, c) = (line[0 .. 2], line[7 .. 9], line[12 .. 14])
+        assert a notin graph
         graph[a] = @[b, c]
 
     var cur = "AAA"
@@ -20,9 +19,34 @@ proc part1(file: File): int =
 
         cur = graph[cur][j]
 
+proc part2(file: File): int =
+    let cmds = file.readLine.toSeq.join(" ").split()
+    discard file.readLine
+
+    var curs: seq[string]
+    var graph = initTable[string, seq[string]]()
+    for line in file.lines:
+        var (a, b, c) = (line[0 .. 2], line[7 .. 9], line[12 .. 14])
+        assert a notin graph
+        graph[a] = @[b, c]
+
+        if a.endsWith("A"):
+            curs.add(a)
+    
+    for i, cmd in cmds.cycle(100000):
+        assert cmd in @["L", "R"]
+        var j: int = (if cmd == "L": 0 else: 1)
+        echo curs
+        if curs.allIt(it.endsWith("Z")):
+            return i
+            
+        for k in 0 ..< curs.len:
+            curs[k] = graph[curs[k]][j]
+        
 const day = "08"
 assert part1(open(fmt"inputs/{day}e1.txt")) == 2
 assert part1(open(fmt"inputs/{day}e2.txt")) == 6
 assert part1(open(fmt"inputs/{day}i.txt")) == 22357
-# assert part2(open(fmt"inputs/{day}e1.txt")) == 5905
+assert part2(open(fmt"inputs/{day}e3.txt")) == 6
+# echo part2(open(fmt"inputs/{day}i.txt"))
 # assert part2(open(fmt"inputs/{day}i.txt")) == 253630098
