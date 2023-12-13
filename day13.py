b1d2ed7b49de028d9aeca3bf25edd48f7741bcd8
Rune Kaagaard <rumi.kg@gmail.com>
2023-12-13 18:28:35 +0100

    Day 13 part 1 done.

--- day13.py
+++ day13.py
@@ -1,52 +1,45 @@
-from copy import deepcopy
-
-# what
-# hfiwegf
-# ghrighi
+def pp(pattern):
+    print("-----------------------------------------")
+    for row in pattern:
+        print("".join(row))
+    print()
 
 def is_perfect(pattern, lineno):
-    d = min(len(pattern) - 1, lineno)
+    for i in range(1, len(pattern)):
+        x, y = lineno - i, lineno + i + 1
+        if x < 0 or y > (len(pattern) - 1):
+            return True
 
-    try:
-        for i in range(1, 10000):
-            if pattern[lineno - i] != pattern[lineno + i + 1]:
-                return False
-    except IndexError:
-        pass
+        if pattern[x] != pattern[y]:
+            return False
 
     return True
 
-def rotate_matrix_clockwise(matrix):
+def rot_clock(matrix):
     return [list(x[::-1]) for x in zip(*matrix)]
 
 def reflexions(patterns, multiplier):
     result = 0
     for i, pattern in enumerate(patterns):
+        ok = False
         for j in range(len(pattern) - 1):
-            # print("".join(pattern[j]))
             if pattern[j] == pattern[j + 1]:
                 if is_perfect(pattern, j):
+                    assert ok is False
+                    ok = True
                     result += (j+1) * multiplier
                     break
 
     return result
 
 def solve(file_name):
-    patterns = [[[z for z in y] for y in x.split("\n")] for x in open(file_name).read().split("\n\n")]
+    patterns = [[[z for z in y] for y in x.split("\n")] for x in open(file_name).read().strip().split("\n\n")]
 
-    patterns2 = deepcopy(patterns)
-    for i in range(len(patterns2)):
-        patterns2[i] = rotate_matrix_clockwise(patterns[i])
-
-    result = reflexions(patterns2, 1)
+    result = reflexions([rot_clock(x) for x in patterns], 1)
     result += reflexions(patterns, 100)
 
     return result
 
 DAY = 13
-
-print(solve(f"inputs/{DAY}e1.txt"))
-print("B")
-print(solve(f"inputs/{DAY}i.txt"))  # 22703 is too low, 30821 is too high
-print("C")
-print("DONE")
+assert solve(f"inputs/{DAY}e1.txt") == 405
+assert solve(f"inputs/{DAY}i.txt") == 28651
